83626f2365fd21f1a21220d3e3afe41ec6a8d54d
Frank <frankpicis96l@gmail>
2023-10-04 05:33:33 -0500

    innit commit

---
+++ app/src/main/java/com/example/apphoroscopo/iu/home/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.apphoroscopo.iu.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.apphoroscopo.R
+import com.example.apphoroscopo.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById( R.id.fcv_main ) as NavHostFragment
+        navController = navHost.navController
+        binding.bnvMain.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/apphoroscopo/iu/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,29 @@
+package com.example.apphoroscopo.iu.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.apphoroscopo.databinding.FragmentHoroscopeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HoroscopeFragment : Fragment() {
+
+    //conectandome al view model
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
